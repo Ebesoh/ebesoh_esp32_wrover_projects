57ce97ab22d7ad0eed71caae68abff375a1f61ca
Ebesoh <ebesoh82@gmail.com>
2026-01-27 16:27:12 +0100

    gpio test updated

--- gpio_test/gpio_loopback_runner.py
+++ gpio_test/gpio_loopback_runner.py
@@ -5,15 +5,19 @@ import gpio_loopback_tests as tests
 def run_all_tests():
     if not tests.loopback_test(14, 19):
         print("CI_RESULT: FAIL (GPIO 14 -> 19)")
-        return 1
+        # return 1
+        sys.exit(1)
 
     elif not tests.loopback_test(12, 18):
         print("CI_RESULT: FAIL (GPIO 12 -> 18)")
-        return 2
+        sys.exit(2)
+        # return 2
     else:
       print("CI_RESULT: PASS")
-      return 0
+      sys.exit(0)
+    #return 0
 
 
 if __name__ == "__main__":
-    sys.exit(run_all_tests())
+    #sys.exit(run_all_tests())
+    run_all_tests()
